7d746b1e35858a0f5cd92b4e2e1754b587039bb0
AlexandreBasei <111810502+AlexandreBasei@users.noreply.github.com>
2025-12-10 16:51:10 +0100

    finished map layout and restart function && add mob spawnPoints and mob spawn cooldown diminution over time

--- main.gd
+++ main.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
 @export var mobs_to_spawn:Array[PackedScene]
+@export var default_spawn_cooldown_diminution:float = 5.0
 
 @onready var spawn_timer = $SpawnTimer
 @onready var player = $Player
+@onready var player_spawn = $PlayerSpawn
 @onready var map = $Map
+@onready var sk_spawwner:Array[Node] = $SkeletonSpawner.get_children()
 @onready var boid_spawner = $BatBoidSpawner
 @onready var boid_timer = $BoidTimer
 @onready var health_bar = $CanvasLayer/HealthBar
+
+var spawn_cooldown_diminution:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,9 +25,23 @@ func _process(delta: float) -> void:
 	pass
 
 func start_game():
-	#spawn_timer.start()
-	boid_timer.start()
+	var mobs:Array[Node] = get_tree().get_nodes_in_group("mobs")
+	
+	for mob in mobs:
+		mob.queue_free()
+	
+	spawn_timer.wait_time = default_spawn_cooldown_diminution
+	spawn_cooldown_diminution = 0.0
+	
+	health_bar._set_health(player.maxHp)
+	player.position = player_spawn.position
+	player.hp = player.maxHp
+	player.current_state = player.PlayerState.Idle
 	player.show()
+	player.shield.show()
+	
+	spawn_timer.start()
+	#boid_timer.start()
 	player.game_started = true
 
 func _on_spawn_timer_timeout() -> void:
@@ -32,22 +51,12 @@ func spawnMob():
 	var mob = mobs_to_spawn[randi_range(0,1)].instantiate()
 	mob.target_player = player
 	mob.tilemap = map
-	var negativeX = randi_range(0,1)
-	var negativeY = randi_range(0,1)
-	
-	if negativeX == 0:
-		if negativeY == 0:
-			mob.position = Vector2(player.position.x + randf_range(150, 200), player.position.y + randf_range(150,200))
-		else:
-			mob.position = Vector2(player.position.x + randf_range(150,200), player.position.y - randf_range(150,200))
-	else:
-		if negativeY == 0:
-			mob.position = Vector2(player.position.x - randf_range(150,200), player.position.y + randf_range(150,200))
-		else:
-			mob.position = Vector2(player.position.x - randf_range(150,200), player.position.y - randf_range(150,200))
+	mob.position = sk_spawwner[randi_range(0, sk_spawwner.size() - 1)].position # Spawn aléatoire entre tous les points de spawn
 	
 	add_child(mob)
-
+	
+	if spawn_timer.wait_time > 1.0 :
+		spawn_timer.wait_time -= spawn_cooldown_diminution + 0.1
 
 func _on_player_player_dead() -> void:
 	spawn_timer.stop()
